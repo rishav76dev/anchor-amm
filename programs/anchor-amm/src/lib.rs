e08ca0572fa9b8f9efb9a57e28d64bd0ee261c15
rishav76dev <rjrishav037@gmail.com>
2025-08-29 05:37:14 +0000

    feat:withdraw

--- programs/anchor-amm/src/lib.rs
+++ programs/anchor-amm/src/lib.rs
@@ -13,6 +13,7 @@ declare_id!("HuMUFVKhxNCEoPKJC5RNySYVLde3foDVTtdHBLVeQzdy");
 
 #[program]
 pub mod anchor_amm {
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, seed: u64, fee: u16, authority: Option<Pubkey>) -> Result<()> {
@@ -28,6 +29,12 @@ pub mod anchor_amm {
 
     pub fn swap(ctx: Context<Swap>, is_x: bool, amount: u64, min: u64) -> Result<()> {
         ctx.accounts.swap(is_x, amount, min)?;
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, min_x: u64, min_y: u64) -> Result<()>{
+        ctx.accounts.withdraw(amount, min_x, min_y)?;
+
         Ok(())
     }
 }
